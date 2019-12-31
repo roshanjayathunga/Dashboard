569ad529b5ae161055f0b3f80b7c6feaee095a61
roshanjayathunga <49506645+roshanjayathunga@users.noreply.github.com>
2019-12-31 10:38:49 +0530

    Initial commit

---
+++ precache-manifest.328a65d32f99216dea0c909441692fd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2823dec68408b4e6dd8e2860a830b74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7195113ed9d38886ccf7",
+    "url": "/static/css/2.c726b981.chunk.css"
+  },
+  {
+    "revision": "f95d09910afb0238f14d",
+    "url": "/static/css/main.ee7cd310.chunk.css"
+  },
+  {
+    "revision": "7195113ed9d38886ccf7",
+    "url": "/static/js/2.936ff9bb.chunk.js"
+  },
+  {
+    "revision": "f95d09910afb0238f14d",
+    "url": "/static/js/main.c449cfa4.chunk.js"
+  },
+  {
+    "revision": "f090625f6b58b45f47d5",
+    "url": "/static/js/runtime-main.3eb7898c.js"
+  }
+]);
